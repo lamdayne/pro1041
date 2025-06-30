c2d8bfe56eee6674ed066ea88260b0cc2cb61b2a
lamdayne <nlam32428@gmail.com>
2025-06-30 09:11:23 +0700

    Save code

---
+++ src/main/java/com/poly/hotel/hotelmanager/Hotelmanager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.poly.hotel.hotelmanager;
+
+/**
+ *
+ * @author PHUONG LAM
+ */
+public class Hotelmanager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! 1");
+    }
+}
