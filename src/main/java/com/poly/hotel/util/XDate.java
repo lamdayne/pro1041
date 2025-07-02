afdb5919546e157a39ceb9c37ebbd92f17093338
lamdayne <nlam32428@gmail.com>
2025-07-02 20:19:19 +0700

    Update util XDate

--- src/main/java/com/poly/hotel/util/XDate.java
+++ src/main/java/com/poly/hotel/util/XDate.java
@@ -4,6 +4,8 @@
  */
 package com.poly.hotel.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -11,5 +13,37 @@ import java.util.Date;
  * @author PHUONG LAM
  */
 public class XDate {
+    public static final String PATTERN_FULL = "yyyy-MM-dd HH:mm:ss";
+    public static final String PATTERN_SHORT = "MM/dd/yyyy";
     
+    private static final SimpleDateFormat formater = new SimpleDateFormat();
+    
+    public static Date now(){
+        return new Date();
+    }
+
+    public static Date parse(String dateTime, String pattern){
+        formater.applyLocalizedPattern(pattern);
+        try {
+            return formater.parse(dateTime);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+    
+    public static Date parse(String dateTime){
+        return parse(dateTime, PATTERN_SHORT);
+    }
+    
+    public static String format(Date dateTime, String pattern){
+        if(dateTime == null){
+            return "";
+        }
+        formater.applyPattern(pattern);
+        return formater.format(dateTime);
+    }
+
+    public static String format(Date dateTime){
+        return format(dateTime, PATTERN_SHORT);
+    }
 }
