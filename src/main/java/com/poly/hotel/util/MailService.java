11a868aeedb684575492e4dcb194cdaf2c2c75f9
lamdayne <nlam32428@gmail.com>
2025-07-16 15:21:08 +0700

    Update MailService

--- src/main/java/com/poly/hotel/util/MailService.java
+++ src/main/java/com/poly/hotel/util/MailService.java
@@ -4,6 +4,7 @@
  */
 package com.poly.hotel.util;
 
+import com.poly.hotel.hotelmanager.MailConstant;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.PasswordAuthentication;
@@ -12,20 +13,19 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-
 /**
  *
  * @author PHUONG LAM
  */
 public class MailService {
-    
+
     private static String sentCode;
 
     public static void sendEmail(String to, String from, String username, String password, String content) {
         Properties prop = new Properties();
         prop.put("mail.smtp.auth", "true");
         prop.put("mail.smtp.starttls.enable", "true");
-        prop.put("mail.smtp.host", "");
+        prop.put("mail.smtp.host", "smtp.gmail.com");
         prop.put("mail.smtp.port", "587");
         prop.put("mail.smtp.socketFactory.port", "587");
         prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
@@ -36,33 +36,35 @@ public class MailService {
                 return new PasswordAuthentication(username, password);
             }
         });
-        
+
         try {
             MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from, "Quản lý khách sạn"));
+            message.setFrom(new InternetAddress(from, "Hotel Manager"));
             message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
-            message.setSubject("Đặt lại mật khẩu");
-            message.setText(content);
+            message.setSubject("Đặt lại mật khẩu", "UTF-8");
+            message.setContent(content, "text/html; charset=UTF-8");
             Transport.send(message);
             System.out.println("Send email success to " + to);
+            MsgBox.alert("Mã xác nhận đã gửi đến email của bạn");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-    
+
     public static String resetCode() {
-        sentCode = String.format("%06d" ,(Math.random() * 1000000));
+        int code = (int) (Math.random() * 1000000);  // ép kiểu double → int
+        sentCode = String.format("%06d", code);     // định dạng 6 chữ số, thêm số 0 phía trước nếu cần
         return sentCode;
     }
-    
+
     public static void sendEmailResetPassword(String email) {
         String from = "quanlykhachsan@gmail.com";
-        String username = "";
-        String password = "";
-        String content = "Mã đặt lại mật khẩu : " + resetCode();
+        String username = MailConstant.email;
+        String password = MailConstant.password;
+        String content = "Mã đặt lại mật khẩu : " + "<strong>" + resetCode() + "</strong>";
         sendEmail(email, from, username, password, content);
     }
-    
+
     public static boolean verifyCode(String input) {
         return sentCode.equals(input);
     }
