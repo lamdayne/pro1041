492dc9f3cff8bf640559ff4d6f932dd7d8bf1bcb
lamdayne <nlam32428@gmail.com>
2025-07-17 21:25:45 +0700

    Add file CustomerController

---
+++ src/main/java/com/poly/hotel/controller/CustomerController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.poly.hotel.controller;
+
+/**
+ *
+ * @author PHUONG LAM
+ */
+public interface CustomerController {
+
+    void open();
+    
+    void fillToTable();
+
+    void checkAll();
+
+    void unCheckAll();
+
+    void deleteCheckedItems();
+}
