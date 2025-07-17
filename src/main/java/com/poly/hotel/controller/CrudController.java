1264e94e8daba358c88146c425898f2e85e08948
lamdayne <nlam32428@gmail.com>
2025-07-17 21:21:32 +0700

    Update CrudController method

--- src/main/java/com/poly/hotel/controller/CrudController.java
+++ src/main/java/com/poly/hotel/controller/CrudController.java
@@ -8,6 +8,31 @@ package com.poly.hotel.controller;
  *
  * @author PHUONG LAM
  */
-public interface CrudController {
-    
+public interface CrudController<Entity> {
+
+    void open(); // Xử lý mở cửa sổ
+
+    void setForm(Entity entity); // Hiển thị thực thể lên form
+
+    Entity getForm(); // Tạo thực thể từ dữ liệu form
+
+    void fillToTable(); // Tải dữ liệu và đổ lên bảng
+
+    void edit(); // Hiển thị dữ liệu của hàng được chọn lên form
+
+    void create(); // Tạo thực thể mới
+
+    void update(); // Cập nhật thực thể đang xem
+
+    void delete(); // Xóa thực thể đang xem
+
+    void clear(); // Xóa trắng form
+
+    void setEditable(boolean editable); // Thay đổi trạng thái form
+
+    void checkAll(); // Tích chọn tất cả các hàng trên bảng
+
+    void uncheckAll(); // Bỏ tích chọn tất cả các hàng trên bảng
+
+    void deleteCheckedItems(); // Xóa các thực thể được tích chọn
 }
