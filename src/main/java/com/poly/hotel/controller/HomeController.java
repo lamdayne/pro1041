54f8566200bb5ec6f91984c444fe24fe23d45dec
lamdayne <nlam32428@gmail.com>
2025-07-17 21:26:03 +0700

    Add file HomeController

---
+++ src/main/java/com/poly/hotel/controller/HomeController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.poly.hotel.controller;
+
+import com.poly.hotel.entity.Room;
+
+/**
+ *
+ * @author PHUONG LAM
+ */
+public interface HomeController extends CrudController<Room> {
+    
+}
