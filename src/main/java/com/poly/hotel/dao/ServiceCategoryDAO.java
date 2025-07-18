fdef6838d12fa81780ed3a63d6beaa5f3eec1e7c
khoa-k2 <khoavu.vndk@gmail.com>
2025-07-18 07:37:20 +0700

    update entity

---
+++ src/main/java/com/poly/hotel/dao/ServiceCategoryDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.poly.hotel.dao;
+
+import com.poly.hotel.entity.ServiceCategory;
+
+/**
+ *
+ * @author PC
+ */
+public interface ServiceCategoryDAO extends CrudDAO<ServiceCategory, String>{
+    
+}
